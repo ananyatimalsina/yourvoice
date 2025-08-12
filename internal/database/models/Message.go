ca1f547b2629ce3ff3f29a7e0cd4a9bba17b3007
Ananya Timalsina <developer@ananyatimalsina.com.np>
2025-08-12 15:23:59 +0200

    Fix db model type mismatch

--- internal/database/models/Message.go
+++ internal/database/models/Message.go
@@ -4,6 +4,6 @@ import "yourvoice/internal/utils"
 
 type Message struct {
 	utils.Expression
-	MessageEventID string `json:"message_event_id" gorm:"not null;unique"`
+	MessageEventID uint   `json:"message_event_id" gorm:"not null;unique"`
 	Message        string `json:"message" gorm:"not null"`
 }
--- internal/database/models/Vote.go
+++ internal/database/models/Vote.go
@@ -4,6 +4,6 @@ import "yourvoice/internal/utils"
 
 type Vote struct {
 	utils.Expression
-	VoteEventID string `json:"vote_event_id" gorm:"not null;unique"`
-	CandidateID string `json:"candidate_id" gorm:"not null"`
+	VoteEventID uint `json:"vote_event_id" gorm:"not null;unique"`
+	CandidateID uint `json:"candidate_id" gorm:"not null"`
 }
