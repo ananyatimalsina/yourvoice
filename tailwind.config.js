96b082b74682a91543eb4a8e0459889cca585d83
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-08-16 09:32:03 +0200

    Add storybook & templ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./web/templates/*.html", "./web/static/main.css"],
+	content: [
+		"./web/templates/*.{templ, templ.go, html}",
+		"./web/static/main.css",
+	],
 	theme: {
 		extend: {},
 	},
