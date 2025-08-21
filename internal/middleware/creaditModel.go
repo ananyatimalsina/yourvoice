38ff25164c38e16674621a870a5c1071f66d53ea
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-08-21 20:33:58 +0200

    middleware: Modify createModel to creaditModel by unify Create and Edit logic

--- internal/middleware/creaditModel.go
+++ internal/middleware/creaditModel.go
@@ -22,20 +22,34 @@ func redirectToOriginal(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, originalURL, http.StatusSeeOther)
 }
 
-func CreateModel(w http.ResponseWriter, r *http.Request, db *gorm.DB, model any) {
+func CreaditModel[T any](w http.ResponseWriter, r *http.Request, db *gorm.DB, model *T) {
+	var request T
+	ctx := r.Context()
+
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Failed to parse form data: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := schema.NewDecoder().Decode(model, r.PostForm); err != nil {
+	if err := schema.NewDecoder().Decode(&request, r.PostForm); err != nil {
 		http.Error(w, "Failed to parse request data: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := db.Create(model).Error; err != nil {
-		http.Error(w, "Failed to create model", http.StatusInternalServerError)
-		return
+	// Check if ID exists
+	id := r.PostForm.Get("id")
+	if id != "" {
+		if _, err := gorm.G[T](db).Where("id = ?", id).Updates(ctx, request); err != nil {
+			http.Error(w, "Failed to edit model", http.StatusInternalServerError)
+			return
+		}
+
+	} else {
+
+		if err := gorm.G[T](db).Create(ctx, &request); err != nil {
+			http.Error(w, "Failed to create model", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	// Redirect to original URL to prevent form resubmission
