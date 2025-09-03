c85c53a0b62253c35358b5f27cec6e6a5db02f89
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-09-03 13:40:18 +0200

    Update developer tooling

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.mkShell {
-  packages = with pkgs; [ go nodejs ];
+  packages = with pkgs; [ go nodejs biome rustywind ];
   shellHook = "export PATH=$PATH:$(go env GOPATH)/bin";
 }
