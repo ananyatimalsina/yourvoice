7897d70de64f0ead82b53a5ea83d79f42d0cc6b6
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-09-05 20:54:07 +0200

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,16 +1,21 @@
 package handlers
 
 import (
-	"github.com/a-h/templ"
-	"gorm.io/gorm"
 	"net/http"
+	"yourvoice/internal/database/models"
 	"yourvoice/internal/handlers/routes/expression"
 	"yourvoice/internal/handlers/routes/identity"
+	"yourvoice/internal/handlers/routes/modelmanagement"
+	"yourvoice/internal/handlers/views/admin/vote"
 	"yourvoice/internal/middleware"
 	"yourvoice/web/templates/admin/pages"
+
+	"github.com/a-h/templ"
+	"github.com/ananyatimalsina/schema"
+	"gorm.io/gorm"
 )
 
-func LoadHanders(router *http.ServeMux, db *gorm.DB) {
+func LoadHanders(router *http.ServeMux, db *gorm.DB, decoder *schema.Decoder) {
 	// Index page
 	//router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	//	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
@@ -27,6 +32,8 @@ func LoadHanders(router *http.ServeMux, db *gorm.DB) {
 		expression.Feedback(w, r, db)
 	})
 
+	router.Handle("/api/expression/", http.StripPrefix("/api/expression", middleware.ContentTypeJson(expressionRouter)))
+
 	identityRouter := http.NewServeMux()
 
 	identityRouter.HandleFunc("POST /verifyVote", func(w http.ResponseWriter, r *http.Request) {
@@ -37,13 +44,30 @@ func LoadHanders(router *http.ServeMux, db *gorm.DB) {
 		identity.VerifyFeedback(w, r, db)
 	})
 
+	router.Handle("/api/identity/", http.StripPrefix("/api/identity", middleware.ContentTypeJson(identityRouter)))
+
 	adminRouter := http.NewServeMux()
 
 	adminRouter.Handle("/", templ.Handler(pages.Dashboard()))
 	adminRouter.Handle("/data/logs", templ.Handler(pages.SystemLogs()))
 
-	router.Handle("/api/expression/", http.StripPrefix("/api/expression", middleware.ContentTypeJson(expressionRouter)))
-	router.Handle("/api/identity/", http.StripPrefix("/api/identity", middleware.ContentTypeJson(identityRouter)))
+	adminVoteRouter := http.NewServeMux()
+
+	// Event management
+	adminVoteRouter.HandleFunc("GET /events", func(w http.ResponseWriter, r *http.Request) {
+		admin_vote.Events(w, r, db)
+	})
+	adminVoteRouter.HandleFunc("DELETE /events", func(w http.ResponseWriter, r *http.Request) {
+		modelmanagement.DeleteModel(w, r, db, &models.VoteEvent{})
+	})
+
+	// Party management
+	admin_vote.RegisterPartyRoutes(adminVoteRouter, db, decoder)
+
+	// Candidate management
+	admin_vote.RegisterCandidateRoutes(adminVoteRouter, db, decoder)
+
+	adminRouter.Handle("/vote/", http.StripPrefix("/vote", adminVoteRouter))
 
 	router.Handle("/admin/", http.StripPrefix("/admin", adminRouter))
 }
