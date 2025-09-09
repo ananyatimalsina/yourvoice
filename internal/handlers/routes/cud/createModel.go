18feee57a6d61716f9e8af6058e66253faa42013
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-09-09 12:35:06 +0200

    modelmanagement: Implement delete and move routes to cud

--- internal/handlers/routes/cud/createModel.go
+++ internal/handlers/routes/cud/createModel.go
@@ -1,4 +1,4 @@
-package modelmanagement
+package cud
 
 import (
 	"github.com/a-h/templ"
--- internal/handlers/routes/cud/deleteModel.go
+++ internal/handlers/routes/cud/deleteModel.go
@@ -1,31 +1,16 @@
-package modelmanagement
+package cud
 
 import (
-	"gorm.io/gorm"
-	"io"
 	"net/http"
-	"net/url"
-)
+	"strings"
 
-type DeleteModelRequest struct {
-	ID uint `json:"id" schema:"id,required"`
-}
+	"gorm.io/gorm"
+)
 
 func DeleteModel[T any](w http.ResponseWriter, r *http.Request, db *gorm.DB, model *T) {
 	ctx := r.Context()
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read body: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-	values, err := url.ParseQuery(string(body))
-	if err != nil {
-		http.Error(w, "Failed to parse form data: "+err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	id := values.Get("id")
+	id := strings.TrimPrefix(r.Header.Get("AJAX-Target"), "row-")
 	if id == "" {
 		http.Error(w, "ID is required for deleting a model", http.StatusBadRequest)
 		return
