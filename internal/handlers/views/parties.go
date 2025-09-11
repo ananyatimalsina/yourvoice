f31ef372c8e7d7d94546375158d57e087096efac
Ananya Timalsina <84459091+ananyatimalsina@users.noreply.github.com>
2025-09-11 13:35:07 +0200

    Remove references to real word parties
    This is done to maintain political impartiality

--- internal/handlers/views/parties.go
+++ internal/handlers/views/parties.go
@@ -1,14 +1,13 @@
 package views
 
 import (
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 	"yourvoice/internal/database/models"
 	"yourvoice/internal/handlers/routes/cud"
 	"yourvoice/web/templates/components/input"
 	"yourvoice/web/templates/modelmanagement"
-
-	"gorm.io/gorm"
 )
 
 func RegisterPartyRoutes(mux *http.ServeMux, db *gorm.DB) {
@@ -19,8 +18,8 @@ func RegisterPartyRoutes(mux *http.ServeMux, db *gorm.DB) {
 		Headers:       []string{"Name", "Candidates", "Created At"},
 		MkRow:         mkRow,
 		ModalProps: modelmanagement.ModalProps{Title: "Party", FormItemProps: []modelmanagement.FormItemProps{
-			{ID: "name", Label: "Name", Placeholder: "Democratic Party", Type: input.TypeText, Required: true, Unique: true, Description: "The name of the party."},
-			{ID: "platform", Label: "Platform", Placeholder: "https://democrats.org/where-we-stand/party-platform/", Type: input.TypeURL, Required: true, Unique: false, Description: "Link to the platform of the party."},
+			{ID: "name", Label: "Name", Placeholder: "Sunshine Party", Type: input.TypeText, Required: true, Unique: true, Description: "The name of the party."},
+			{ID: "platform", Label: "Platform", Placeholder: "https://sunshine.example.com/platform", Type: input.TypeURL, Required: true, Unique: false, Description: "Link to the platform of the party."},
 		},
 		}}
 
